f6a4ff6dae2b281cd268141f5d9760e815665429
Nelson Crosby <nc@sourcecomb.com>
2014-08-28 21:20:24 +1200

    Hello, World!

---
+++ src/main/groovy/io/github/nelsoncrosby/gridrace/GridRace.groovy
@@ -0,0 +1,15 @@
+package io.github.nelsoncrosby.gridrace
+
+/**
+ *
+ */
+class GridRace {
+
+    /**
+     * Entry point.
+     * @param args
+     */
+    static void main(String[] args) {
+        println 'Hello, World!'
+    }
+}
