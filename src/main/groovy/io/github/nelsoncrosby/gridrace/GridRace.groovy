c1d8ca18f5832914524e15a0cdae693e93170f7c
Nelson Crosby <nc@sourcecomb.com>
2014-08-28 21:44:41 +1200

    Slick2D window.

--- src/main/groovy/io/github/nelsoncrosby/gridrace/GridRace.groovy
+++ src/main/groovy/io/github/nelsoncrosby/gridrace/GridRace.groovy
@@ -1,15 +1,42 @@
 package io.github.nelsoncrosby.gridrace
 
+import org.newdawn.slick.AppGameContainer
+import org.newdawn.slick.BasicGame
+import org.newdawn.slick.GameContainer
+import org.newdawn.slick.Graphics
+import org.newdawn.slick.SlickException
+
 /**
  *
  */
-class GridRace {
+class GridRace extends BasicGame {
+
+    GridRace(String title) {
+        super(title)
+    }
+
+    @Override
+    void init(GameContainer gameContainer) throws SlickException {
+        println 'init'
+    }
+
+    @Override
+    void update(GameContainer gameContainer, int i) throws SlickException {
+        print 'update\r'
+    }
+
+    @Override
+    void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+        print 'render\r'
+    }
 
     /**
      * Entry point.
-     * @param args
+     * @param args Command-line arguments
      */
     static void main(String[] args) {
-        println 'Hello, World!'
+        AppGameContainer appgc = new AppGameContainer(new GridRace("GridRace"))
+        appgc.setDisplayMode(640, 480, false)
+        appgc.start()
     }
 }
