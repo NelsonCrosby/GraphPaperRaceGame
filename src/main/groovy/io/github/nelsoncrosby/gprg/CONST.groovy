1f6a05257f010e1f5b42cdae2fbbeac26779de2f
Nelson Crosby <nc@sourcecomb.com>
2014-09-17 13:47:51 +1200

    Swapped CONST.UP and CONST.DOWN They were in the wrong order before.

--- src/main/groovy/io/github/nelsoncrosby/gprg/CONST.groovy
+++ src/main/groovy/io/github/nelsoncrosby/gprg/CONST.groovy
@@ -8,9 +8,9 @@ class CONST {
     static final int RIGHT_BUTTON = 1
     static final int MIDDLE_BUTTON = 2
     static final int LEFT = -1
-    static final int UP = -1
+    static final int UP = 1
     static final int RIGHT = 1
-    static final int DOWN = 1
+    static final int DOWN = -1
 
     static final String TITLE = 'Graph Paper Race Game'
 }
